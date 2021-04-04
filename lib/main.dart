28abc8967f7d7176cbb838bbdcbbf237110aa39c
yodyindra <indrayody04@gmail.com>
2021-04-04 20:31:14 +0800

    membuat list dgn menggunakan class DummyListView

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,35 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Hello'),
           ),
+          body: DummyListView(),
         ));
+  }
+}
+
+class DummyListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var lists = [];
+
+    for (var i = 0; i < 50; i++) {
+      lists.add('Hello ' + (i + 1).toString());
+    }
+
+    return ListView.builder(
+      padding: const EdgeInsets.all(1),
+      itemCount: lists.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 80,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[Text('${lists[index]}')],
+          ),
+          decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey))),
+        );
+      },
+    );
   }
 }
